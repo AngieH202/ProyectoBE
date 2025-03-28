38777db54f86bf46d04e5c1d46b85a0558d22f8c
AngieH202 <hernandez4.angie@gmail.com>
2025-03-28 12:35:17 -0600

    no message

---
+++ BackEndAngie/BackEndAngie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackEndAngie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
